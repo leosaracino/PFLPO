f864735863bf656f529f9fb1e82de8dead6b6ddc
leo saracino <leosaracino2211@gmail.com>
2023-07-13 13:33:35 -0300

    adicionando ordenação e arrumando pagina de recordes

--- assets/js/tabela.js
+++ assets/js/tabela.js
@@ -9,6 +9,12 @@ document.addEventListener('DOMContentLoaded', function() {
       const exercicio = document.getElementById('exercicio').value;
       const peso = document.getElementById('peso').value;
 
+      // Validação de entrada numérica para o campo de peso
+      if (!isNumeric(peso)) {
+         alert("Por favor, insira um valor numérico para o peso.");
+         return;
+      }
+
       // Crie um objeto com os valores
       const recorde = { data, exercicio, peso };
 
@@ -30,15 +36,63 @@ document.addEventListener('DOMContentLoaded', function() {
       tabela.innerHTML = '';
 
       // Adicione os registros na tabela
-      recordes.forEach(function(recorde) {
+      recordes.forEach(function(recorde, index) {
          const row = tabela.insertRow();
          const cellNome = row.insertCell();
          const cellPeso = row.insertCell();
          const cellData = row.insertCell();
+         const cellExcluir = row.insertCell();
 
          cellNome.textContent = recorde.exercicio;
          cellPeso.textContent = recorde.peso;
          cellData.textContent = recorde.data;
+
+         // Adicione o botão de exclusão
+         const botaoExcluir = document.createElement('button');
+         botaoExcluir.classList.add('btn', 'btn-danger', 'btn-sm');
+         botaoExcluir.innerHTML = '<i class="fas fa-trash"></i>';
+         botaoExcluir.addEventListener('click', function() {
+            excluirRecorde(index);
+         });
+         cellExcluir.appendChild(botaoExcluir);
       });
    }
+
+   // Função para excluir um recorde
+   function excluirRecorde(index) {
+      recordes.splice(index, 1);
+      atualizarTabelaRecordes();
+   }
+
+   // Função auxiliar para verificar se um valor é numérico
+   function isNumeric(value) {
+      return !isNaN(parseFloat(value)) && isFinite(value);
+   }
+
+   // Função para organizar a lista de recordes
+   function ordenarRecordes(criterio) {
+      recordes.sort(function(a, b) {
+         if (criterio === 'nome') {
+            return a.exercicio.localeCompare(b.exercicio);
+         } else if (criterio === 'peso') {
+            return parseFloat(a.peso) - parseFloat(b.peso);
+         } else if (criterio === 'data') {
+            return new Date(a.data) - new Date(b.data);
+         }
+      });
+      atualizarTabelaRecordes();
+   }
+
+   // Manipuladores de clique dos botões de organização
+   document.getElementById('btnOrdenarNome').addEventListener('click', function() {
+      ordenarRecordes('nome');
+   });
+
+   document.getElementById('btnOrdenarPeso').addEventListener('click', function() {
+      ordenarRecordes('peso');
+   });
+
+   document.getElementById('btnOrdenarData').addEventListener('click', function() {
+      ordenarRecordes('data');
+   });
 });
